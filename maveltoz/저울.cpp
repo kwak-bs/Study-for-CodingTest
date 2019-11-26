0a0f1b8cde7787d6a5177bdf8a89b466f91bdb7f
Dae-Hoon, KIM <eogns0707@gmail.com>
2019-11-26 20:26:10 +0900

    Update 저울.cpp

--- maveltoz/저울.cpp
+++ maveltoz/저울.cpp
@@ -1,52 +1,3 @@
-/*
-https://programmers.co.kr/learn/courses/30/lessons/42886
-*/
-
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
-
-int ans;
-vector<int> weight;
-
-void inputData() {
-	cin.get();
-	while (1) {
-		int k;
-		char c;
-		cin >> k;
-		weight.push_back(k);
-		cin >> c;
-		if (c == ']') break;
-		cin.get();
-	}
-}
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0), cout.tie(0);
-
-	inputData();
-
-	sort(weight.begin(), weight.end());
-
-	if (weight[0] > 1) {
-		cout << 1;
-		exit(0);
-	}
-
-	for (int i = 0; i < weight.size(); i++) {
-		ans += weight[i];
-		if (i != weight.size() - 1 && ans + 1 < weight[i + 1]) break;
-	}
-
-	cout << ++ans;
-
-	return 0;
-}
-
-/*
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -69,4 +20,3 @@ int solution(vector<int> weight) {
 
 	return ++ans;
 }
-*/
