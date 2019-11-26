37fd12dfd2a8faf15f7ad88ba3c12e34104807af
rojae <41769568+rojae@users.noreply.github.com>
2019-11-26 11:53:06 +0900

    Add files via upload

---
+++ rojae/그리디알고리즘/큰수만들기.cpp
@@ -0,0 +1,46 @@
+﻿/*
+ * https://programmers.co.kr/learn/courses/30/lessons/42883
+ */
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+string solution(string number, int k) {
+	int n = number.size();
+	k = n - k;
+	string ans;
+
+	// Maximum value, least k from last index
+
+	int max_idx = -1;
+	for (int flag = 0; flag < k; flag++) {
+		int cur_max = 0;
+		for (int i = max_idx + 1; i <= n - k + flag; i++) {
+			int value = number[i];
+			if (cur_max < value) {
+				cur_max = value;
+				max_idx = i;
+				if (value == '9')	
+					break;
+			}
+		}
+		ans += to_string(cur_max - '0');
+	}
+
+	return ans;
+}
+
+int main() {
+	string number;
+	int k;
+
+	cin >> number >> k;
+	cout << solution(number, k);
+	
+	return 0;
+}
+
