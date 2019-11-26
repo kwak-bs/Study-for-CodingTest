20589380277791dcee7ee43cd62ff66aad8fe7a2
rojae <41769568+rojae@users.noreply.github.com>
2019-11-27 00:06:32 +0900

    Add files via upload

---
+++ rojae/그리디알고리즘/저울.cpp
@@ -0,0 +1,32 @@
+﻿/*
+ * https://programmers.co.kr/learn/courses/30/lessons/42886
+ */
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> weight) {
+	int ans = 0;
+
+	sort(weight.begin(), weight.end());
+
+	if (weight[0] > 1)
+		return 1;
+
+	for(int i = 0; i < weight.size(); i++) {
+		ans += weight[i];
+		if (ans + 1 < weight[i + 1] && i != weight.size())
+			break;
+	}
+
+	return ans + 1;
+}
+
+int main() {
+	vector<int> weight = { 3,1,6,2,7,30,1 };
+	cout << solution(weight);
+	return 0;
+}
