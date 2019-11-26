e23021194271fa7b31ce86964327560a25012d95
Dae-Hoon, KIM <eogns0707@gmail.com>
2019-11-26 20:24:40 +0900

    Update 구명보트.cpp

--- maveltoz/구명보트.cpp
+++ maveltoz/구명보트.cpp
@@ -1,49 +1,3 @@
-/*
-https://programmers.co.kr/learn/courses/30/lessons/42885
-*/
-
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
-
-vector<int> people;
-int limit, flag, ans;
-
-void inputData() {
-	cin.get();
-	while (1) {
-		int weight;
-		cin >> weight;
-		people.push_back(weight);
-		char c;
-		cin >> c;
-		if (c == ']') break;
-	}
-	cin >> limit;
-}
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0), cout.tie(0);
-
-	inputData();
-
-	sort(people.begin(), people.end());
-
-	for (int i = people.size() - 1; i >= flag; i--) {
-		if (people[flag] + people[i] <= limit) {
-			flag++;
-		}
-		ans++;
-	}
-
-	cout << ans;
-
-	return 0;
-}
-
-/*
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -63,4 +17,3 @@ int solution(vector<int> people, int limit) {
 
 	return ans;
 }
-*/
