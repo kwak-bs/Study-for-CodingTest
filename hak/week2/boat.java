0e89f77fa848b0514da1d4e9ff8551f8bc82c54b
YuuuuuuYu <37291245+YuuuuuuYu@users.noreply.github.com>
2019-11-26 19:45:04 +0900

    Create boat.java

---
+++ hak/week2/boat.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+    public static int solution(int[] people, int limit) {
+		Arrays.sort(people);
+		
+		int result = people.length;
+		int j = people.length-1;
+		for (int i = 0; j > i; j--) {
+			int heavy = people[j];
+			if (people[i] + heavy <= limit) {
+				result--;
+				i++;	
+			}					
+		}
+		
+		return result;
+    }
+}
