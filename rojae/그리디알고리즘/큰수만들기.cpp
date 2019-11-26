8035c7f914501008f1b4413a0d991c56bdef90eb
rojae <41769568+rojae@users.noreply.github.com>
2019-11-26 20:29:09 +0900

    Update 큰수만들기.cpp

--- rojae/그리디알고리즘/큰수만들기.cpp
+++ rojae/그리디알고리즘/큰수만들기.cpp
@@ -11,23 +11,33 @@ using namespace std;
 
 string solution(string number, int k) {
 	int n = number.size();
-	k = n - k;
-	string ans;
+	k = n - k;			// k 재정의 사용할 수 있는 수의 갯수
+	string ans;			// 정답 string
 
 	// Maximum value, least k from last index
-
 	int max_idx = -1;
-	for (int flag = 0; flag < k; flag++) {
+	
+	// flag는 자리수를 의미한다
+	// flag 0은 가장 큰 자리 수
+	for (int flag = 0; flag < k; flag++) {	
 		int cur_max = 0;
+		
+		// max_idx : 직전에 처리한 숫자의 위치
+		// i는 직전에 처리한 수보다 뒤에 위치하게 된다.
+		// n - k + flag : 1092에서 2개의 숫자를 추출할 경우
+		// 		: 최대 값인 9는 뒤에 숫자가 최소 하나가 있어야한다
 		for (int i = max_idx + 1; i <= n - k + flag; i++) {
 			int value = number[i];
 			if (cur_max < value) {
 				cur_max = value;
 				max_idx = i;
+				
+				// 효율성 문제 해결
 				if (value == '9')	
 					break;
 			}
 		}
+		// 아스키코드를 숫자로 바꾼다
 		ans += to_string(cur_max - '0');
 	}
 
