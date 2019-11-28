c5be0909833c08857c3b0ab248b964f323efdbea
YuuuuuuYu <37291245+YuuuuuuYu@users.noreply.github.com>
2019-11-29 08:27:27 +0900

    Create MoreSpicy.java

---
+++ hak/Heap/MoreSpicy.java
@@ -0,0 +1,11 @@
+// 더 맵게
+public class MoreSpicy {
+	public int solution(int[] scoville, int K) {
+        int answer = 0;
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
