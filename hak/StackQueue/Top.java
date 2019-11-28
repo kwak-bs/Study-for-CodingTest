7281b098c777d45e31010c1618ba2faa62d408fc
YuuuuuuYu <37291245+YuuuuuuYu@users.noreply.github.com>
2019-11-29 03:02:35 +0900

    Update Top.java

--- hak/StackQueue/Top.java
+++ hak/StackQueue/Top.java
@@ -6,13 +6,13 @@ public class Top {
 	public static int[] solution(int[] heights) {
 		 int[] answer = new int[heights.length];
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < heights.length; i++)			// 송신 탑 높이를 Stack에 Push
+        for (int i = 0; i < heights.length; i++)// 송신 탑 높이를 Stack에 Push
         	stack.push(heights[i]);
         
         ListIterator<Integer> it;		// Stack의 원소 출력용
-        int element, top, index;			// element: 적절한 수신 탑, 
-        											// top: 현재 스택 젤 맨 위의 값, 
-        											// index: element 조절 값
+        int element, top, index;		// element: 적절한 수신 탑, 
+        					// top: 현재 스택 젤 맨 위의 값, 
+        					// index: element 조절 값
         
         // 송신 탑의 수신을 받을 수 있는 수신 탑 검색
         for (int i = stack.size()-1; !stack.isEmpty(); i--) {	// i: 현재 송신 탑
