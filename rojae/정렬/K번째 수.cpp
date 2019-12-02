134a937562648acf6a7ba4f725823b2a9a25d83c
rojae <41769568+rojae@users.noreply.github.com>
2019-12-03 00:18:53 +0900

    Add files via upload

---
+++ rojae/정렬/K번째 수.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+	vector<int> answer;
+	
+	for (int i = 0; i < commands.size(); i++) {
+		int start = commands[i][0];
+		int end = commands[i][1];
+		int k = commands[i][2];
+
+		vector<int> tmp(end - start + 1);
+
+		copy(array.begin() + start - 1, array.begin() + end, tmp.begin());
+		
+		sort(tmp.begin(), tmp.end());
+		answer.push_back(tmp[k-1]);
+	}
+	return answer;
+}
+
+int main() {
+	vector<int> a = { 1, 5, 2, 6, 3, 7, 4 };
+	vector<vector<int>> c = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+	vector<int> ans;
+
+	ans = solution(a, c);
+	
+	for (int i : ans)
+		cout << i;
+
+	return 0;
+ }
